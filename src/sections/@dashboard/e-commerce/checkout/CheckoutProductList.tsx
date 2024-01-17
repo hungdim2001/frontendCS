3d0031421e37de76b2ff768044c0b40ba229c670
hungdim2001 <hungdim2001@gmail.com>
2024-01-17 17:31:16 +0700

    17h 1/17/2023

--- src/sections/@dashboard/e-commerce/checkout/CheckoutProductList.tsx
+++ src/sections/@dashboard/e-commerce/checkout/CheckoutProductList.tsx
@@ -13,6 +13,7 @@ import {
   TableContainer,
   Card,
   Grid,
+  Stack,
 } from '@mui/material';
 // utils
 import getColorName from '../../../../utils/getColorName';
@@ -22,6 +23,7 @@ import { CartItem } from '../../../../@types/product';
 // components
 import Image from '../../../../components/Image';
 import Iconify from '../../../../components/Iconify';
+import SvgIconStyle from 'src/components/SvgIconStyle';
 
 // ----------------------------------------------------------------------
 
@@ -29,10 +31,13 @@ const IncrementerStyle = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
-  marginBottom: theme.spacing(0.5),
-  padding: theme.spacing(0.5, 0.75),
-  borderRadius: theme.shape.borderRadius,
-  border: `solid 1px ${theme.palette.grey[500_32]}`,
+  // marginBottom: theme.spacing(0.5),
+  padding: theme.spacing(0.5),
+  color: '#717171',
+  fontSize: '14px',
+  fontWeight: 300,
+  // borderRadius: theme.shape.borderRadius,
+  borderBottom: `solid 1px ${theme.palette.grey[500]}`,
 }));
 
 // ----------------------------------------------------------------------
@@ -55,115 +60,87 @@ export default function CheckoutProductList({
       {products.map((product, index) => {
         const { variant, name, quantity } = product;
         return (
-          <>
-            <Card key={index} sx={{ padding: 2 }}>
-              <Grid container >
-                <Grid item lg={3} md={4}>  <Image
+          <Card key={index} sx={{ padding: 2, boxShadow: '0 4px 4px 0  rgba(0, 0, 0, .25)' }}>
+            <Grid container>
+              <Grid item lg={3} md={3}>
+                {' '}
+                <Image
                   alt="product image"
                   src={variant.image.toString()}
-                // sx={{ width: 64, height: 64, borderRadius: 1.5, mr: 2 }}
-                /></Grid>
-                <Grid item lg={9} md={8} container
-                  direction="column"
-                  justifyContent="space-between"
-                  alignItems="flex-start"
-                >
-                  <Typography noWrap variant="h6" >
-                    {name}
-                  </Typography>
-                  <Grid>  {Object.entries(variant.charValues).map(([key, value], index) => (
-                    <Typography key={index} sx={{
-                      fontSize: 14,
-                      fontWeight: 400,
-                      color: '#717170'
-                    }}>
+                  // sx={{ width: 64, height: 64, borderRadius: 1.5, mr: 2 }}
+                />
+              </Grid>
+              <Grid
+                item
+                lg={9}
+                md={9}
+                container
+                direction="column"
+                justifyContent="space-between"
+                alignItems="flex-start"
+              >
+                <Typography noWrap variant="h6">
+                  {name}
+                </Typography>
+                <Grid>
+                  {' '}
+                  {Object.entries(variant.charValues).map(([key, value], index) => (
+                    <Typography
+                      key={index}
+                      sx={{
+                        fontSize: 14,
+                        fontWeight: 500,
+                        color: '#717170',
+                      }}
+                    >
                       <Typography
                         component="span"
                         variant="subtitle1"
                         sx={{
-                          color: 'text.primary', fontSize: 14,
+                          color: 'text.primary',
+                          fontSize: 14,
                           fontWeight: 400,
-                          mt:'0.5'
                         }}
                       >
                         {key}:&nbsp;
                       </Typography>
                       {value}
                     </Typography>
-                  ))}</Grid>
-
-                  <Typography
-                    variant='body1'
-                  >{fCurrency(variant.price)}
-                  </Typography>
+                  ))}
                 </Grid>
-
-                <Grid item lg={3}></Grid>
-              </Grid>
-
-
-
-            </Card>
-
-            {/* <TableRow key={variant.id}>
-              <TableCell>
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-
-                  <Box>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                      }}
+                <Grid container direction="row" justifyContent="space-between">
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    {variant.price && (
+                      <Typography
+                        component="span"
+                        sx={{ color: 'text.disabled', textDecoration: 'line-through' }}
+                      >
+                        {fCurrency(variant.price)}₫
+                      </Typography>
+                    )}
+                    <Typography variant="body1">{fCurrency(variant.price)}₫</Typography>
+                  </Stack>
+                  <Stack direction="row" alignItems="center" spacing={1}>
+                    <IconButton
+                      sx={{ color: '#C91433', mt: '12px', padding: 0 }}
+                      onClick={() => onDelete(variant.id)}
                     >
-                   </Box>
-                  </Box>
-                </Box>
-              </TableCell>
-
-              <TableCell align="left">{fCurrency(variant.price)}</TableCell>
-
-              <TableCell align="left">
-                <Incrementer
-                  quantity={quantity}
-                  available={variant.quantity}
-                  onDecrease={() => onDecreaseQuantity(variant.id)}
-                  onIncrease={() => onIncreaseQuantity(variant.id)}
-                />
-              </TableCell>
-
-              <TableCell align="right">{fCurrency(variant.price * quantity)}</TableCell>
-
-              <TableCell align="right">
-                <IconButton onClick={() => onDelete(variant.id)}>
-                  <Iconify icon={'eva:trash-2-outline'} width={20} height={20} />
-                </IconButton>
-              </TableCell>
-            </TableRow> */}
-
-          </>
-
-
+                      <SvgIconStyle src={'/icons/ic_trash.svg'} />
+                    </IconButton>
+                    <Incrementer
+                      quantity={quantity}
+                      available={variant.quantity}
+                      onDecrease={() => onDecreaseQuantity(variant.id)}
+                      onIncrease={() => onIncreaseQuantity(variant.id)}
+                    />
+                  </Stack>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Card>
         );
       })}
-
     </>
-    // <TableContainer sx={{ minWidth: 720 }}>
-    //   <Table>
-    //     {/* <TableHead>
-    //       <TableRow>
-    //         <TableCell>Product</TableCell>
-    //         <TableCell align="left">Price</TableCell>
-    //         <TableCell align="left">Quantity</TableCell>
-    //         <TableCell align="right">Total Price</TableCell>
-    //         <TableCell align="right" />
-    //       </TableRow>
-    //     </TableHead> */}
-
-    //     <TableBody>
-    //    </TableBody>
-    //   </Table>
-    // </TableContainer>
   );
 }
 
@@ -193,9 +170,9 @@ function Incrementer({ available, quantity, onIncrease, onDecrease }: Incremente
           <Iconify icon={'eva:plus-fill'} width={16} height={16} />
         </IconButton>
       </IncrementerStyle>
-      <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+      {/* <Typography variant="caption" sx={{ color: 'text.secondary' }}>
         available: {available}
-      </Typography>
+      </Typography> */}
     </Box>
   );
 }
